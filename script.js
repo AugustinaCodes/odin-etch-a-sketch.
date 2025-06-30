ed164262f0c24cb7a5be12ccd1bf8a0e0419fd1b
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-30 16:49:46 +0300

    Add hovering affect and user prompt

--- script.js
+++ script.js
@@ -11,4 +11,21 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(column)
 }
 
-const cell = document.querySelector(".square")
+const cells = document.querySelectorAll(".square")
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add("hovered")
+    })
+})
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    const answer = prompt("Enter number of squares per side you'd like your nre grid to be")
+    const number = Number(answer)
+
+    if(number > 100) {
+        prompt("Please enter a number smaller than a 100")
+    }
+    console.log(typeof number);
+    
+})
