27160dc1c9ae2d55469d428967fdb8d7cd1cf317
August <76185140+Augustina1995@users.noreply.github.com>
2025-07-01 15:52:49 +0300

    Generate new grid via user prompt

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div")
-    column.classList.add("column")
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div")
-        cell.classList.add("square")
-        column.appendChild(cell)
+function generateGrid(userChoice) {
+  for (let i = 0; i < userChoice; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 0; j < userChoice; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("square");
+      column.appendChild(cell);
     }
-    container.appendChild(column)
+    container.appendChild(column);
+  }
 }
 
-const cells = document.querySelectorAll(".square")
+const cells = document.querySelectorAll(".square");
 cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add("hovered")
-    })
-})
+  cell.addEventListener("mouseover", () => {
+    cell.classList.add("hovered");
+  });
+});
 
-const button = document.querySelector("button")
+const button = document.querySelector("button");
 button.addEventListener("click", () => {
-    const answer = prompt("Enter number of squares per side you'd like your nre grid to be")
-    const number = Number(answer)
+  const answer = prompt(
+    "Enter number of squares per side you'd like your new grid to be"
+  );
+  const number = Number(answer);
 
-    if(number > 100) {
-        prompt("Please enter a number smaller than a 100")
-    }
-    console.log(typeof number);
-    
-})
+  if (number > 100) {
+    const notification = document.createElement("div");
+    notification.textContent = "Please enter a number smaller than a 100";
+    notification.id = "notif";
+    document.body.appendChild(notification);
+  } else {
+    document.querySelector("#notif").remove();
+    generateGrid(number);
+  }
+});
